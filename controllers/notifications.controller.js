441c07ef3f1a7fed8b4866ffcfe31e266df28496
lucasgojeda <lukitas.ojeda.00@gmail.com>
2022-12-04 22:50:29 -0300

    Filters on notifications module has been implemented

--- controllers/notifications.controller.js
+++ controllers/notifications.controller.js
@@ -8,23 +8,30 @@ const getNotifications = async (req = request, res = response) => {
 
     const { id } = req;
 
+    const { page } = req.params;
+
+    const desde = page * 8 - 8;
+    const limite = page * 8;
     try {
 
         const user = await User.findById(id);
 
-
         const [sales, notifications] = await Promise.all([
-            Sale.find({ user: user._id }).populate('user', 'name'),
+            Sale.find({ user: user._id })
+                .populate('user', 'name'),
             Notification.find({ user: user._id })
         ])
 
-        // let filteredSales = sortArray(sales, orderBy, filterBy);
+        let filteredSales = sortArray(sales, 'desc', 'date_sended');
+
+        let filteredNotifications = []
+
+        notifications.forEach((e, i) => (i > (desde -1) && i < limite) && (filteredNotifications = [...filteredNotifications, e]))
 
         res.json({
             msg: 'OK',
-            // sales: filteredSales,
-            sales,
-            notifications
+            sales: filteredSales,
+            notifications: filteredNotifications
         })
 
     } catch (error) {
--- routes/notifications.routes.js
+++ routes/notifications.routes.js
@@ -11,7 +11,7 @@ const {
 
 const router = Router();
 
-router.get('/', [
+router.get('/:page', [
     jwtValidate,
     validateFields
 ], getNotifications);
